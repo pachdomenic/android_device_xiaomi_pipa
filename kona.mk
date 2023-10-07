8ef7981639f2e006a41d0b9b4befae62eb3fd3ec
madmax7896 <madmax78968@gmail.com>
2023-10-07 23:24:26 +0530

    pipa: Import Dolby audio blobs
    Change-Id: If15323d1f6139ff347abc5e0138f3b87322fcf6b

--- kona.mk
+++ kona.mk
@@ -218,6 +218,9 @@ PRODUCT_PACKAGES += \
     media_codecs_performance_kona.xml \
     video_system_specs.json
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
+
 # OMX
 PRODUCT_PACKAGES += \
     libOmxCore \
