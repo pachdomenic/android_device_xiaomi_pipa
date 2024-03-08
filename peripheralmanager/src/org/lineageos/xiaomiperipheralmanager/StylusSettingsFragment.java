8644a77347493cfd24473813e026870d8f61a124
Michael Bestas <mkbestas@gmail.com>
2024-03-08 20:15:39 +0200

    pipa: peripheralmanager: Convert to SwitchPreferenceCompat
    Change-Id: Ic1cbaba37d499da1855af9c3930f2df426e2d3af

--- peripheralmanager/src/org/lineageos/xiaomiperipheralmanager/StylusSettingsFragment.java
+++ peripheralmanager/src/org/lineageos/xiaomiperipheralmanager/StylusSettingsFragment.java
@@ -12,7 +12,7 @@ import android.os.Bundle;
 import android.os.SystemProperties;
 import android.preference.PreferenceManager;
 import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 import com.android.settingslib.widget.FooterPreference;
 import com.android.settingslib.widget.MainSwitchPreference;
 import java.text.SimpleDateFormat;
@@ -48,8 +48,8 @@ public class StylusSettingsFragment extends PreferenceFragment implements
     stylusModePref.setChecked(
         mStylusPreference.getBoolean("stylus_mode_key", false));
 
-    SwitchPreference forceRecognizePref =
-        (SwitchPreference)findPreference("force_recognize_stylus_key");
+    SwitchPreferenceCompat forceRecognizePref =
+        (SwitchPreferenceCompat)findPreference("force_recognize_stylus_key");
     forceRecognizePref.setChecked(
         mStylusPreference.getBoolean("force_recognize_stylus_key", false));
   }
