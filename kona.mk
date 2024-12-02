ed51218cb8cd7289a2486513f24ce70fec08d46e
Flicker372 <flicker372@outlook.com>
2024-12-02 19:57:11 +0800

    pipa: Bring back OMX
    This reverts commit 931a013de75d9aea4e82e85de01a7920c9810caf.
    
    Change-Id: I37f1cd6b307468c40fba6c3b733ab273d5e945ca

--- kona.mk
+++ kona.mk
@@ -214,10 +214,21 @@ $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
     media_codecs_performance_c2.xml \
+    media_codecs_kona.xml \
+    media_codecs_performance_kona.xml \
     video_system_specs.json
 
 # OMX
-TARGET_SUPPORTS_OMX_SERVICE := false
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
