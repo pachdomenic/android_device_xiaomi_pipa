68970aa46e49ab1c7de04081a0bb48b9b522702d
pachadomenic <pachadomenic2010@gmail.com>
2024-12-04 22:16:23 +0200

    pipa: Update fingerprint to V816.0.12.0.UMZMIXM
    Change-Id: I80d25e02036afccc9539e8e3af07ec93f10ce227

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -25,4 +25,4 @@ PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
- BuildFingerprint=Xiaomi/pipa_global/pipa:13/TKQ1.221114.001/V14.0.8.0.TMZMIXM:user/release-keys
+ BuildFingerprint=Xiaomi/pipa_global/pipa:14/UKQ1.230917.001/V816.0.12.0.UMZMIXM:user/release-keys
