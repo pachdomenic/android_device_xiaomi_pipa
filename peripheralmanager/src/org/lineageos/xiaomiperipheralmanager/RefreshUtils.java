a88837c7586298de791a91f99f2cc17da279ca63
pachadomenic <pachadomenic2010@gmail.com>
2025-02-11 21:34:41 +0200

    Revert "peripheralmanager: Detect Pen events and update refresh rate appropriately"

--- peripheralmanager/src/org/lineageos/xiaomiperipheralmanager/RefreshUtils.java
+++ peripheralmanager/src/org/lineageos/xiaomiperipheralmanager/RefreshUtils.java
@@ -3,16 +3,12 @@ package org.lineageos.xiaomiperipheralmanager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.provider.Settings;
-import android.util.Log;
 
 public final class RefreshUtils {
-    private static final String TAG = "RefreshUtils";
     private static final String KEY_PEAK_REFRESH_RATE = "peak_refresh_rate";
     private static final String KEY_MIN_REFRESH_RATE = "min_refresh_rate";
     private static final String KEY_PEN_MODE = "pen_mode";
     private static final String PREF_FILE_NAME = "pen_refresh_prefs";
-
-    private static final boolean DEBUG = true;
 
     private Context mContext;
     private SharedPreferences mSharedPrefs;
@@ -23,46 +19,36 @@ public final class RefreshUtils {
     }
 
     protected void setPenRefreshRate() {
-        if (DEBUG) Log.d(TAG, "Setting pen Refresh Rate");
         boolean penMode = mSharedPrefs.getBoolean(KEY_PEN_MODE, false);
-        if (DEBUG) Log.d(TAG, "setPenRefreshRate: penMode = " + penMode);
 
-        float maxRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, 144f);
-        float minRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, 144f);
+        if (!penMode) {
+            float maxRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, 144f);
+            float minRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, 60f);
 
-        if (DEBUG) Log.d(TAG, "setPenRefreshRate: maxRate = " + maxRate + ", minRate = " + minRate);
+            // Update default values in SharedPreferences
+            mSharedPrefs.edit()
+                    .putFloat(KEY_MIN_REFRESH_RATE, minRate)
+                    .putFloat(KEY_PEAK_REFRESH_RATE, maxRate)
+                    .putBoolean(KEY_PEN_MODE, true)
+                    .apply();
 
-        // Update default values in SharedPreferences
-        mSharedPrefs.edit()
-                .putFloat(KEY_MIN_REFRESH_RATE, minRate)
-                .putFloat(KEY_PEAK_REFRESH_RATE, maxRate)
-                .putBoolean(KEY_PEN_MODE, true)
-                .apply();
+            // Ensure valid values for maxRate and minRate
+            maxRate = (maxRate != 60) ? 120 : maxRate;
+            minRate = (minRate <= 60) ? 60 : 120;
 
-        if (DEBUG) Log.d(TAG, "setPenRefreshRate: SharedPreferences updated with values ^^^.");
-
-        // Ensure valid values for maxRate and minRate
-        maxRate = (maxRate != 60) ? 120 : maxRate;
-        minRate = (minRate <= 60) ? 60 : 120;
-
-        if (DEBUG) Log.d(TAG, "setPenRefreshRate: Validated maxRate = " + maxRate + ", minRate = " + minRate);
-
-        // Set the values in the Settings.System
-        Settings.System.putFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, minRate);
-        Settings.System.putFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, maxRate);
-
-        if (DEBUG) Log.d(TAG, "setPenRefreshRate: Settings.System updated");
+            // Set the values in the Settings.System
+            Settings.System.putFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, minRate);
+            Settings.System.putFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, maxRate);
+        }
     }
 
     protected void setDefaultRefreshRate() {
-        if (DEBUG) Log.d(TAG, "Setting default Refresh Rate");
         float defaultMinRate = mSharedPrefs.getFloat(KEY_MIN_REFRESH_RATE, 144f);
-        float defaultMaxRate = mSharedPrefs.getFloat(KEY_PEAK_REFRESH_RATE, 144f);
-
-        if (DEBUG) Log.d(TAG, "setDefaultRefreshRate: defaultMinRate = " + defaultMinRate + ", defaultMaxRate = " + defaultMaxRate);
+        float defaultMaxRate = mSharedPrefs.getFloat(KEY_PEAK_REFRESH_RATE, 60f);
 
         mSharedPrefs.edit().putBoolean(KEY_PEN_MODE, false).apply();
-        if (DEBUG) Log.d(TAG, "setDefaultRefreshRate: SharedPreferences updated to disable pen mode");
+
+        // Set the values in the Settings.System directly
         Settings.System.putFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, defaultMinRate);
         Settings.System.putFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, defaultMaxRate);
     }
