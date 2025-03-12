e992deedb3f2973b30321bd35967525c524c96c9
kenway214 <kenway214@outlook.com>
2025-03-13 02:45:36 +0530

    pipa: Introduce GameBar v2.0
    Change-Id: I028907ada386890efdd36c4f127d22e56c0e3dcd
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>
    Signed-off-by: CuriousNom <aryanbiswas7585@gmail.com>

--- kona.mk
+++ kona.mk
@@ -324,6 +324,7 @@ PRODUCT_PACKAGES += \
     init.device.rc \
     init.qcom.power.rc \
     init.qcom.rc \
+    init.pipa.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi.rc \
