7b61f4c2204ba3113aaaecbd0c31f2e67f6711d4
Arian <arian.kulmer@web.de>
2025-08-23 08:28:49 +0300

    pipa: Don't explicitly build libwfdaac_vendor
    This has been converted to blueprint and is built implicitly as
    dependency of libwfdmmsrc_proprietary.so.
    
    Change-Id: Ib89a88866ac81c206b46b948b7b0f346d4928a3e

--- kona.mk
+++ kona.mk
@@ -331,8 +331,5 @@ PRODUCT_COPY_FILES += \
 # Wi-Fi Display
 $(call inherit-product, device/qcom/wfd/device.mk)
 
-PRODUCT_PACKAGES += \
-    libwfdaac_vendor
-
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/pipa/pipa-vendor.mk)
