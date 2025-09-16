d1b4681c364c4d83273510b3acfdbb9c71651f50
CuriousNom <aryanbiswas7585@gmail.com>
2025-09-17 00:35:10 +0530

    pipa: Revert "Get rid of XiaomiParts"
    This reverts commit d7d2428c59887f73304b9580025243b056dd7094.

--- kona.mk
+++ kona.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
