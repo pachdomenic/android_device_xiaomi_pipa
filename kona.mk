532b184e28eb8c837dc338a33b8ae1d78e303577
LuK1337 <LuK1337@users.noreply.github.com>
2025-12-10 22:09:34 +0530

    pipa: Mirgrate to lineagehealth IFastCharge
    Change-Id: I7f3a2e9c4b8d1f6a5e0c9b7d3f2a1e8c4b6d9f5a

--- kona.mk
+++ kona.mk
@@ -234,9 +234,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti
 
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/qcom-battery/restrict_chg)
+$(call soong_config_set,lineage_health,fast_charge_value_none,1)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
+
 # GMS
 WITH_GMS_COMMS_SUITE := false
 USE_TABLET_BT_COD := true
+
+ifneq ($(TARGET_IS_VAB),true)
+PRODUCT_PACKAGES += \
+    android.hardware.health-service.qti_recovery
+endif
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
