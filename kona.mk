c9ed752255590fa74830dcd30961f9cef9a5714c
CuriousNom <aryanbiswas7585@gmail.com>
2025-12-18 15:01:06 +0530

    pipa: Convert overlays to RRO
    Ref: LineageOS/android_device_xiaomi_sm8250-common@1f60b5d
    
    Change-Id: I8c3388f91e17244d854578b0c8aac3159b21da40

--- kona.mk
+++ kona.mk
@@ -324,9 +324,16 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+PRODUCT_PACKAGES += \
+    ApertureOverlayDevice \
+    FrameworkResOverlayDevice \
+    LineageSDKOverlayDevice \
+    LineageSettingsOverlayDevice \
+    LineageSystemUIOverlayDevice \
+    SettingsOverlayDevice \
+    SettingsProviderOverlayDevice \
+    SystemUIOverlayDevice \
+    WifiResDevice
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -430,7 +437,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     NcmTetheringOverlay \
-    WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
 
