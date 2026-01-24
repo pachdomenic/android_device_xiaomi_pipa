0b7e77e67f96b25ca64150d0c9bcda6b1f12cc69
Mathieu Chartier <mathieuc@google.com>
2026-01-24 11:53:51 +0530

    pipa: Reduce system server verbosity
    Change-Id: I58d2e993ae9a7213e94661d22b47c28b0a2243e8

--- kona.mk
+++ kona.mk
@@ -203,6 +203,9 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# Reduce system server verbosity
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+
 # Disable Scudo outside of eng builds to save RAM.
 PRODUCT_DISABLE_SCUDO := true
 
