989e5132a291f2a08be4aac83070434e9e9aac2d
Michael Bestas <mkbestas@lineageos.org>
2026-06-22 03:46:06 +0300

    pipa: Update WFD system blobs from CPH2747_11_C_OTA_1020_all_6yqNTQ_01000100
    Change-Id: I733c2bf342175474ce7f02d467a17ded6f0a8760

--- extract-files.py
+++ extract-files.py
@@ -45,8 +45,11 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .replace_needed('libaudioclient.so', 'libaudiobase.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/etc/init/init.batterysecret.rc': blob_fixup()
         .regex_replace(r'\s+seclabel u:r:batterysecret:s0', ''),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
