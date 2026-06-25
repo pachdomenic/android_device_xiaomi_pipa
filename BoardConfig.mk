756f70a5cd1e2f5694fb97136de40195dfb0b5fd
Tuki <pachadomenic2010@gmail.com>
2026-06-26 00:54:42 +0300

    pipa: Switch back to source-built kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,18 +93,18 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+# TARGET_KERNEL_NO_GCC := true
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
+TARGET_KERNEL_VERSION := 4.19
+TARGET_KERNEL_CONFIG := \
+    pipa_defconfig \
+    vendor/xiaomi/magictime-common.config \
 
-# Kernel - prebuilt
-INLINE_KERNEL_BUILDING := false
-TARGET_NO_KERNEL_OVERRIDE := true
-TARGET_FORCE_PREBUILT_KERNEL := true
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)-kernel/kernel
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)-kernel/dtb.img
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)-kernel/dtbo.img
+    
+# Kernel Clang Flags
+# KERNEL_CC := CC=clang
+# override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
 
-PRODUCT_COPY_FILES += \
-    $(TARGET_PREBUILT_KERNEL):kernel \
-    $(TARGET_PREBUILT_DTB):dtb.img
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
+#    ["kernel/xiaomi/pipa"]="https://github.com/CuriousNom/n0_kernel_pipa"
 # Define repositories and their target directories
 declare -A REPOS=(
     ["vendor/xiaomi/pipa"]="https://github.com/Matrixx-Devices/vendor_xiaomi_pipa"
     ["vendor/xiaomi/pipa-firmware"]="https://codeberg.org/CuriousNom/proprietary_vendor_xiaomi_pipa-firmware"
-    ["kernel/xiaomi/pipa"]="https://github.com/CuriousNom/n0_kernel_pipa"
 )
 
 # Continue with other repos
@@ -16,25 +15,6 @@ for DIR in "${!REPOS[@]}"; do
         git clone --depth 1 "${REPOS[$DIR]}" "$DIR" || { echo "[ERROR] Failed to clone ${REPOS[$DIR]}"; exit 1; }
     fi
 done
-
-# Prebuilt kernel
-PIPA_KERNEL_DIR="device/xiaomi/pipa-kernel"
-PIPA_KERNEL_REPO="https://github.com/Matrixx-Devices/device_xiaomi_pipa-kernel.git"
-PIPA_KERNEL_BRANCH="n0-bpf"
-
-if [ -d "$PIPA_KERNEL_DIR" ]; then
-    # Check if it's the correct repo
-    if git -C "$PIPA_KERNEL_DIR" remote get-url origin 2>/dev/null | grep -q "$PIPA_KERNEL_REPO"; then
-        echo "[INFO] device/xiaomi/pipa-kernel is already tracking the correct repo, skipping..."
-    else
-        echo "[INFO] device/xiaomi/pipa-kernel is tracking a different repo. Replacing it..."
-        rm -rf "$PIPA_KERNEL_DIR"
-        git clone --depth 1 -b "$PIPA_KERNEL_BRANCH" "$PIPA_KERNEL_REPO" "$PIPA_KERNEL_DIR" || { echo "[ERROR] Failed to clone pipa-kernel"; exit 1; }
-    fi
-else
-    echo "[INFO] Cloning device/xiaomi/pipa-kernel..."
-    git clone --depth 1 -b "$PIPA_KERNEL_BRANCH" "$PIPA_KERNEL_REPO" "$PIPA_KERNEL_DIR" || { echo "[ERROR] Failed to clone pipa-kernel"; exit 1; }
-fi
 
 # Hardware/xiaomi
 HW_XIAOMI_DIR="hardware/xiaomi"
